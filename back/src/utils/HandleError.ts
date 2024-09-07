13c2cc7250524cc800fd4b042e35333cee07b83a
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-09-07 12:40:10 +0200

    handling errors

--- back/src/utils/HandleError.ts
+++ back/src/utils/HandleError.ts
@@ -1,8 +1,14 @@
+import { Logger } from '../middlewares';  // Import du logger Winston
 import type { ErrorObject } from '../types';
 
 export class HandleError {
+    /**
+     * Ensure the error is an instance of Error.
+     * @param {unknown} error - The error to ensure.
+     * @returns {Error} The error instance.
+     */
     public static ensureError(error: unknown): Error {
-        if(error instanceof Error) return error;
+        if (error instanceof Error) return error;
 
         let stringify: string = 'Unable to stringify error.';
 
@@ -13,6 +19,13 @@ export class HandleError {
         return new Error(`An unexpected error occurred: ${stringify}`);
     }
 
+
+
+    /**
+     * Handle an error object.
+     * @param {ErrorObject} errorObject - The error object to handle.
+     * @returns {Error} The error instance.
+     */
     public static handle(errorObject: ErrorObject): Error {
         const errorInstance: Error = HandleError.ensureError(errorObject.error);
 
@@ -22,6 +35,8 @@ export class HandleError {
             errorObject.message ? ` - ${errorObject.message}` : '',
             `: ${errorInstance.message}`
         ].filter(Boolean).join('');
+
+        Logger.error(formattedError, { stack: errorInstance.stack });
 
         return errorInstance;
     }
